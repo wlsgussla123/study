a6c8ffe350ce405c88530f9b77e356611729bd5d
Jinhyeon Park <wlsgussla123@gmail.com>
2017-10-24 01:12:15 +0900

    2. Gradle에 3rd party 추가
    Gradle이랑 npm이랑 비슷한? 느낌을 받고있다.

--- Gradle/GradleStudy/src/main/java/org/gradle/GradleStudy.java
+++ Gradle/GradleStudy/src/main/java/org/gradle/GradleStudy.java
@@ -1,7 +1,12 @@
 package org.gradle;
 
+import org.joda.time.LocalTime;
+
 public class GradleStudy {
 	public static void main(String[] args) {
+		LocalTime currentTime = new LocalTime();
+		System.out.println("The current local time is: " + currentTime);
+		
 		Greeter greeter = new Greeter();
 		System.out.print(greeter.sayHello());
 	}
