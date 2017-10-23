147d33a2e7660efc768a5e0569b8ebe06e498640
Jinhyeon Park <wlsgussla123@gmail.com>
2017-10-24 00:51:13 +0900

    Gradle - 1. 기본 동작 공부
    https://www.journaldev.com/8179/gradle-tutorial
    https://spring.io/guides/gs/gradle/

---
+++ Gradle/GradleStudy/src/main/java/org/gradle/GradleStudy.java
@@ -0,0 +1,8 @@
+package org.gradle;
+
+public class GradleStudy {
+	public static void main(String[] args) {
+		Greeter greeter = new Greeter();
+		System.out.print(greeter.sayHello());
+	}
+}
---
+++ Gradle/GradleStudy/src/main/java/org/gradle/Greeter.java
@@ -0,0 +1,7 @@
+package org.gradle;
+
+public class Greeter {
+	public String sayHello() {
+		return "Hello world!";
+	}
+}
