5532d9e4614c2291a2fd9e8275960e46711bd751
Jinhyeon Park <wlsgussla123@gmail.com>
2017-10-27 12:38:08 +0900

    중복 코드의 메서드 추출
    관심사의 분리

--- Spring/SpringStudy/src/main/java/com/jinhyeon/demo/dao/UserDao.java
+++ Spring/SpringStudy/src/main/java/com/jinhyeon/demo/dao/UserDao.java
@@ -3,18 +3,25 @@ package com.jinhyeon.demo.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.jinhyeon.demo.domain.User;
 
-public class UserDao {	
-	String dbUrl = "jdbc:oracle:thin:@localhost:1521:xe";
-	String id = "jinhyeon";
-	String pwd = "****";
-	
-	public void add(User user) throws ClassNotFoundException, SQLException {
+public class UserDao {		
+	private Connection getConnection() throws ClassNotFoundException, SQLException {
+		String dbUrl = "jdbc:oracle:thin:@localhost:1521:xe";
+		String id = "jinhyeon";
+		String pwd = "****";
+
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		Connection conn = DriverManager.getConnection(dbUrl, id, pwd);
+		
+		return conn;
+	}
+	
+	public void add(User user) throws ClassNotFoundException, SQLException {
+		Connection conn = getConnection();
 		
 		PreparedStatement pstmt = conn.prepareStatement("insert into users(id, name, password) values (?,?,?)");
 		pstmt.setString(1,  user.getId());
@@ -28,7 +35,22 @@ public class UserDao {
 	}
 	
 	public User get(String id) throws ClassNotFoundException, SQLException {
+		Connection conn = getConnection();
+		
+		PreparedStatement pstmt = conn.prepareStatement("select * from users where id = ?");
+		pstmt.setString(1,id);
+		
+		ResultSet rs = pstmt.executeQuery();
+		rs.next();
+		
 		User user = new User();
+		user.setId(rs.getString("id"));
+		user.setName(rs.getString("name"));
+		user.setPassword(rs.getString("password"));
+		
+		rs.close();
+		pstmt.close();
+		conn.close();
 		
 		return user;
 	}
