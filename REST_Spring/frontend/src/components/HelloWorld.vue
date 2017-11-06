8d6fc36893fbf8558b0e86d853c57e3b0fd1613b
Jinhyeon Park <wlsgussla123@gmail.com>
2017-11-06 14:39:18 +0900

    front, vue로 기본셋팅

---
+++ REST_Spring/frontend/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <h1 class="h1">{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.hello {
+  h1 {
+    color: red
+  }
+}
+</style>
